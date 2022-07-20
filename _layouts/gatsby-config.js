226b39558747f5cf117691a5029d71a5f9794b9c
Yoghurt M <yoghurt@tuta.io>
2022-07-20 16:18:41 -0400

    gatsby-theme-garden is not working now

--- _layouts/gatsby-config.js
+++ _layouts/gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: `gatsby-theme-garden`,
+      resolve: `gatsby-garden`,
       options: {
         rootNote: "/readme",
         contentPath: `${__dirname}/..`,
