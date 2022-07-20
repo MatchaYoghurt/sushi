03602f9067621179253dc70136c9edb22ef33c83
Yoghurt M <yoghurt@tuta.io>
2022-07-20 16:01:27 -0400

    use gatsby-theme-garden

--- _layouts/gatsby-config.js
+++ _layouts/gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: `gatsby-digital-garden`,
+      resolve: `gatsby-theme-garden`,
       options: {
         rootNote: "/readme",
         contentPath: `${__dirname}/..`,
